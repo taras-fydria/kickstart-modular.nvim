b291f0dc9f00bee4089c77139023ba1dcc0448f7
TarasFydriaDelphi <Taras.Fydria@udelphi.com>
2024-09-11 22:04:09 +0300

    setup ts and small visual fixes

---
+++ lua/custom/configs/noice.lua
@@ -0,0 +1,26 @@
+local noice = require 'noice'
+
+noice.setup {
+  routes = {
+    {
+      view = 'notify',
+      filter = { event = 'msg_showmode' },
+    },
+  },
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+      ['vim.lsp.util.stylize_markdown'] = true,
+      ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  presets = {
+    -- you can enable a preset by setting it to true, or a table that will override the preset config
+    -- you can also add custom presets that you can enable/disable with enabled=true
+    bottom_search = false, -- use a classic bottom cmdline for search
+    command_palette = false, -- position the cmdline and popupmenu together
+    long_message_to_split = false, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+  },
+}
